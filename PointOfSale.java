e70b4ccc009391aab193484b8c85d973fa904b0e
Riotpunchbeats <Ayden.Green@me.com>
2017-03-21 20:21:27 -0400

    Starting preamble

--- PointOfSale.java
+++ PointOfSale.java
@@ -7,8 +7,9 @@
  * 1. Declare variables
  * 2. Declare constants
  * 3. Create Arrays 
- * 4. Get user input
- * 5. Create loop
+ * 4. Print Preamble
+ * 5. Get user input
+ * 6. Create loop
  */
 
 import java.text.NumberFormat;
@@ -20,6 +21,11 @@ public class PointOfSale
 	{
 		//Create a Scanner object
 		Scanner input = new Scanner(System.in);
+		String custname = "";
+
+		System.out.println("Hi! Welcome to Maggie's! \nWhat's your name?");
+		custname = input.next();
+		System.out.println("Ok, " + custname + " , enter the price of each of your purchases in dollars and cents and then hit the ENTER key. \nFor example, if your item cost $5.99, then you would enter 5.99.");
 
     }
 }
