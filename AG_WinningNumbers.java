71454dc1e301742918926b9ca3c0cba136ff00a6
Riotpunchbeats <Ayden.Green@me.com>
2017-03-28 12:17:58 -0400

    ?

---
+++ AG_WinningNumbers.java
@@ -0,0 +1,23 @@
+ /**
+ * Program Name:	AG_WinnningNumbers.java
+ * Purpose:			
+ * Coder:			Ayden Green 0818753
+ * Date:				March, 28, 2017
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AG_WinnningNumbers
+{
+	public static void main(String[] args)
+	{
+        //User intro
+        System.out.println("Welcome to Ayden Green Quick Pick Lotto Simulation! ");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Instructions:");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("1. Purchase your ticket for $6.50 n/2. Pick 10 numbers between 0-9 for your ticket. n/3. Your ticket will be comparead to the winning numbers.");
+
+    }
+
+}
