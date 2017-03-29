de65914aa8d6f9b44a47050ac454e6146584c765
Riotpunchbeats <Ayden.Green@me.com>
2017-03-29 02:37:57 -0400

    deleted some shit

--- PointOfSale.java
+++ PointOfSale.java
@@ -36,15 +36,4 @@ public class PointOfSale
 
 		
     }
-public static void fillArray (String[] args)
-{
-    Scanner input = new Scanner(System.in);
-    double[] numbers = new double[10];
-
-    for (int i = 0; i < numbers.length; i++)
-    {
-        System.out.println("Please enter number");
-        numbers[i] = input.nextDouble();
-    }
-}
-}
+} 
